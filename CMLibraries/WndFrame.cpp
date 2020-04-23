2f29a57033c8d04ebf0dc7e1c7bd312e421e62ea
Hotege <myownroc@163.com>
2020-04-23 22:55:12 +0800

    add centerize for main frame window

--- CMLibraries/WndFrame.cpp
+++ CMLibraries/WndFrame.cpp
@@ -22,6 +22,8 @@ CWndFrame::CWndFrame()
 		CW_USEDEFAULT, CW_USEDEFAULT, FRAME_DEFAULT_WIDTH, FRAME_DEFAULT_HEIGHT,
 		nullptr, nullptr, ::GetModuleHandle(nullptr), this);
 	m_pSysMenu = new CWndSysMenu(this);
+	int cx = GetSystemMetrics(SM_CXSCREEN), cy = GetSystemMetrics(SM_CYSCREEN);
+	SetWindowPos(m_hWnd, 0, (cx - FRAME_DEFAULT_WIDTH) / 2, (cy - FRAME_DEFAULT_HEIGHT) / 2, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
 }
 
 
